11bc5bb0772de8c48c2738fbf0248d1cf0b2603f
srjizhere <bhiapatil@gmail.com>
2023-02-21 06:54:56 +0530

    added some

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,13 @@ const path = require('path')
 
 
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 
 app.use("/api/user",userRoutes)
 app.use('/api/chat',chatRoutes)
